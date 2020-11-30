9f54adc25eccedbdd8a2ba0621abd77670b39f57
Kim Yongdae <ydkim2110@gmail.com>
2020-11-30 20:57:58 +0900

    finishing up profile ui

--- Instagram Clone/View/Profile/ProfileCell.swift
+++ Instagram Clone/View/Profile/ProfileCell.swift
@@ -11,6 +11,13 @@ class ProfileCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    private let postImageView: UIImageView = {
+       let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
     
     // MARK: - Lifecycle
     
@@ -19,6 +26,9 @@ class ProfileCell: UICollectionViewCell {
         super.init(frame: frame)
         
         backgroundColor = .lightGray
+        
+        addSubview(postImageView)
+        postImageView.fillSuperview()
     }
     
     required init?(coder: NSCoder) {
