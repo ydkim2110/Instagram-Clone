2fe7a002a037a3c3c91613ed07f99e26afacb29e
Kim Yongdae <ydkim2110@gmail.com>
2020-11-30 18:37:25 +0900

    commit missing files

---
+++ Instagram Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram Clone
+//
+//  Created by 김용대 on 2020/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
