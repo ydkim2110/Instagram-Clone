019cca501656b1d296c7f2bc04bcbb5629b30483
Kim Yongdae <ydkim2110@gmail.com>
2020-11-30 20:33:03 +0900

    setting up profile collection view

---
+++ Instagram Clone/View/Profile/ProfileCell.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileCell.swift
+//  Instagram Clone
+//
+//  Created by 김용대 on 2020/11/30.
+//
+
+import UIKit
+
+class ProfileCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .lightGray
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
---
+++ Instagram Clone/View/Profile/ProfileHeader.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileHeader.swift
+//  Instagram Clone
+//
+//  Created by 김용대 on 2020/11/30.
+//
+
+import UIKit
+
+class ProfileHeader: UICollectionReusableView {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
