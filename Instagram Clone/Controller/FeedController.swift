612a18e66af80ce4e6b8b3b5bafa0efb6254df1f
Kim Yongdae <ydkim2110@gmail.com>
2020-12-07 21:29:13 +0900

    upload like info to Firestone

--- Instagram Clone/Controller/FeedController.swift
+++ Instagram Clone/Controller/FeedController.swift
@@ -157,7 +157,14 @@ extension FeedController: FeedCellDelegate {
         if post.didLike {
             print("DEBUG: Unlike post here...")
         } else {
-            print("DEBUG: Like post here...")
+            PostService.likePost(post: post) { error in
+                if let error = error {
+                    print("DEBUG: like post error...")
+                }
+                
+                cell.likeButton.setImage(#imageLiteral(resourceName: "like_selected"), for: .normal)
+                cell.likeButton.tintColor = .red
+            }
         }
     }
     
