e138db128010e02b18eb70045dd563daf6fe9980
Kim Yongdae <ydkim2110@gmail.com>
2020-12-08 12:17:53 +0900

    notification service

---
+++ Instagram Clone/API/NotificationService.swift
@@ -0,0 +1,44 @@
+//
+//  NotificationService.swift
+//  Instagram Clone
+//
+//  Created by 김용대 on 2020/12/08.
+//
+
+import Firebase
+
+struct NotificationService {
+    
+    static func uploadNotification(
+        toUid uid: String,
+        type: NotificationType,
+        post: Post? = nil
+    ) {
+    
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        guard uid != currentUid else { return }
+        
+        let docRef =
+            COLLECTION_NOTIFICATIONS.document(uid).collection("user-notifications").document()
+        
+        var data: [String : Any] = [
+            "timestamp" : Timestamp(date: Date()),
+            "uid" : currentUid,
+            "type" : type.rawValue,
+            "id" : docRef.documentID
+        ]
+        
+        if let post = post {
+            data["postId"] = post.postId
+            data["postImageUrl"] = post.imageUrl
+        }
+        
+        docRef.setData(data)
+        
+    }
+    
+    static func fetchNotification() {
+        
+    }
+    
+}
