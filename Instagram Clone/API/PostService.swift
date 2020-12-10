65393f14427d1ea553c190df5be1cbd88c25d94c
Kim Yongdae <ydkim2110@gmail.com>
2020-12-10 12:24:17 +0900

    update user feed after unfollow and post

--- Instagram Clone/API/PostService.swift
+++ Instagram Clone/API/PostService.swift
@@ -29,7 +29,9 @@ struct PostService {
                 "ownerUsername" : user.username
             ] as [String : Any]
             
-            COLLECTION_POSTS.addDocument(data: data, completion: completion)
+            let docRef = COLLECTION_POSTS.addDocument(data: data, completion: completion)
+            
+            self.updateUserFeedAfterPost(postId: docRef.documentID)
         }
     }
     
@@ -144,7 +146,7 @@ struct PostService {
         
     }
     
-    static func updateUserFeedAfterFollowing(user: User) {
+    static func updateUserFeedAfterFollowing(user: User, didFollow: Bool) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         let query = COLLECTION_POSTS.whereField("ownerUid", isEqualTo: user.uid)
@@ -154,11 +156,33 @@ struct PostService {
             let docIDs = documents.map({ $0.documentID })
         
             docIDs.forEach { id in
-                COLLECTION_USERS.document(uid).collection("user-feed")
-                    .document(id).setData([:])
+                if didFollow {
+                    COLLECTION_USERS.document(uid).collection("user-feed")
+                        .document(id).setData([:])
+                } else {
+                    COLLECTION_USERS.document(uid).collection("user-feed")
+                        .document(id).delete()
+                }
             }
             
         }
+    }
+    
+    static func updateUserFeedAfterPost(postId: String) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        COLLECTION_FOLLOWERS.document(uid).collection("user-followers")
+            .getDocuments { (snapshot, _) in
+                guard let documents = snapshot?.documents else { return }
+                
+                documents.forEach { document in
+                    COLLECTION_USERS.document(document.documentID).collection("user-feed")
+                        .document(postId).setData([:])
+                }
+                
+                COLLECTION_USERS.document(uid).collection("user-feed")
+                    .document(postId).setData([:])
+            }
     }
     
 }
